aaabb5fc3700e33bd1f260586128433fa1118901
Andres <andresbailo98n@gmail.com>
2025-10-14 13:48:34 +0200

    pilas y colas

---
+++ Listas/src/colas/Cabecera3.java
@@ -0,0 +1,29 @@
+package colas;
+
+
+
+public class Cabecera3 {
+	Tupla3ObjetosCircular primero;
+	int tamano=0;
+
+	public Cabecera3(Tupla3ObjetosCircular primero) {
+
+		this.primero = primero;
+	}
+
+	public int getTamano() {
+		return tamano;
+	}
+
+	public void setTamano(int tamano) {
+		this.tamano = tamano;
+	}
+
+	public Tupla3ObjetosCircular getPrimero() {
+		return primero;
+	}
+
+	public void setPrimero(Tupla3ObjetosCircular primero) {
+		this.primero = primero;
+	}
+}
---
+++ Listas/src/colas/Lista6Version.java
@@ -0,0 +1,82 @@
+package colas;
+
+import java.util.Scanner;
+
+public class Lista6Version {
+	Cabecera3 cabecera;
+
+	public Lista6Version() {
+
+	}
+
+	public void insertar(int elemento) {
+
+		Tupla3ObjetosCircular tupla = new Tupla3ObjetosCircular(elemento);
+
+		if (cabecera == null) {
+
+			cabecera = new Cabecera3(tupla);
+			tupla.setSiguienteElemento(tupla);
+			tupla.setPosicionAnterior(tupla);
+		} else {
+
+			Tupla3ObjetosCircular ultimo = cabecera.getPrimero().getPosicionAnterior();
+
+			ultimo.setSiguienteElemento(tupla);
+			tupla.setPosicionAnterior(ultimo);
+			tupla.setSiguienteElemento(cabecera.getPrimero());
+			cabecera.getPrimero().setPosicionAnterior(tupla);
+		}
+
+		cabecera.setTamano(cabecera.getTamano() + 1);
+	}
+
+	public void mostrar() {
+
+		Tupla3ObjetosCircular actual = cabecera.getPrimero();
+		do {
+			System.out.println(
+					"Elemento: " + actual.getElemento() + " | Anterior: " + actual.getPosicionAnterior().getElemento()
+							+ " | Siguiente: " + actual.getSiguienteElemento().getElemento());
+
+			actual = actual.getSiguienteElemento();
+		} while (actual != cabecera.getPrimero());
+	}
+
+	public void sacar() {
+		
+		
+		cabecera.getPrimero().getPosicionAnterior().setSiguienteElemento(cabecera.getPrimero().getSiguienteElemento());;
+		cabecera.setPrimero(cabecera.getPrimero().getSiguienteElemento());
+		
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Lista6Version l = new Lista6Version();
+		while (true) {
+			System.out.println("elige: 1 insertar, 2 mostrar, 3 eliminar");
+			int input = sc.nextInt();
+			switch (input) {
+			case 1: {
+
+				System.out.println("elemento");
+				int elemento = sc.nextInt();
+				l.insertar(elemento);
+				break;
+			}
+			case 2: {
+				l.mostrar();
+				break;
+			}
+			case 3: {
+				
+				l.sacar();
+				break;
+			}
+			}
+
+		}
+	}
+}
